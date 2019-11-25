6688768bc83b1d84c9d4252f1b280c11158257b2
Yugo Nishimura <e195719@ie.u-ryukyu.ac.jp>
2019-11-25 14:28:49 +0900

    できそう

---
+++ src/test/jp/ac/uryukyu/ie/e195719/EnemyTest.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e195719;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyTest {
+
+    @Test
+    void attack() {
+        short expecteds = 10;
+        short actuals = 15;
+        assertEquals(expecteds, actuals);
+    }
+}
